643f8c817c0a7e7ce05dddf513b7182aea024fc5
ShaAli <sali1@stuy.edu>
2015-12-03 20:34:13 -0500

    2

--- Comparable.java
+++ Comparable.java
@@ -1,1 +1,8 @@
+// Team Blah -- Shanjeed Ali, Shamaul Dilmohamed
+// APCS1 pd10
+// HW41 -- In America, the Driver Sits on the Left
+// 2015-12-04
 
+public interface Comparable {
+    int compareTo(Object o);
+}
